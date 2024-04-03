159e6776749410dcf8bf36431f91058b3655a33a
Francis Ng <francis-ng@outlook.com>
2024-04-03 14:11:36 +0900

    PWA additions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+import withSerwistInit from '@serwist/next';
+
+const withSerwist = withSerwistInit({
+  swSrc: 'app/sw.ts',
+  swDest: 'public/sw.js'
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withSerwist({
+  nextConfig
+});
---
+++ src/app/sw.ts
@@ -0,0 +1,18 @@
+import { defaultCache } from "@serwist/next/worker";
+import type { PrecacheEntry } from "@serwist/precaching";
+import { Serwist } from "@serwist/sw";
+
+declare const self: ServiceWorkerGlobalScope & {
+  // Change this attribute's name to your `injectionPoint`.
+  // `injectionPoint` is an InjectManifest option.
+  // See https://serwist.pages.dev/docs/build/inject-manifest/configuring
+  __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
+};
+
+new Serwist().install({
+  precacheEntries: self.__SW_MANIFEST,
+  skipWaiting: true,
+  clientsClaim: true,
+  navigationPreload: true,
+  runtimeCaching: defaultCache,
+});
