b4c43bc7941d9759fc582f2b10cfe5c1637e1845
Francis Ng <francis-ng@outlook.com>
2024-04-03 14:37:13 +0900

    Fix PWA config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 import withSerwistInit from '@serwist/next';
 
 const withSerwist = withSerwistInit({
-  swSrc: 'app/sw.ts',
+  swSrc: 'src/app/sw.ts',
   swDest: 'public/sw.js'
 })
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withSerwist({
-  nextConfig
-});
+export default withSerwist(nextConfig);
