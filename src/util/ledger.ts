884b859045d78d57c6ffa2313d81c8bbb77fe3b4
Francis Ng <github@fnha.anonaddy.com>
2025-12-01 17:02:28 +0900

    Switch back to using email

--- src/util/ledger.ts
+++ src/util/ledger.ts
@@ -27,7 +27,7 @@ export async function listLedgers(): Promise<SerializableLedger[]> {
   const headerData = await headers();
   if (!headerData) return [];
   const session = await auth.api.getSession({ headers: headerData });
-  const user = session.user.id;
+  const user = session.user.email;
   console.log(`Listing ledgers for ${user}`);
   const client = await clientPromise;
   const ledgers = client.db(process.env.MONGO_DB).collection<Ledger>(process.env.LEDGER_COLLECTION);
@@ -45,7 +45,7 @@ export async function listLedgers(): Promise<SerializableLedger[]> {
 
 export async function getLedger(year:number, month:number): Promise<SerializableLedger> {
   const session = await auth.api.getSession({ headers: await headers() });
-  const user = session.user.id;
+  const user = session.user.email;
   console.log(`Getting ledger ${year}/${month} for ${user}`);
   const client = await clientPromise;
   const ledgers = client.db(process.env.MONGO_DB).collection<Ledger>(process.env.LEDGER_COLLECTION);
