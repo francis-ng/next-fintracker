e401f36d2c6c5ddfd9ff75549feaafb0ecfe3d34
Francis Ng <github@fnha.anonaddy.com>
2025-12-04 16:25:00 +0900

    Add explicit config for stateless auth

--- src/auth.ts
+++ src/auth.ts
@@ -2,6 +2,18 @@ import { betterAuth, Auth } from "better-auth"
 
 export const auth: Auth = betterAuth({
   secret: process.env.BETTER_AUTH_SECRET,
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 7 * 24 * 60 * 60, // 7 days
+      strategy: 'jwe',
+      refreshCache: true
+    }
+  },
+  account: {
+    storeStateStrategy: 'cookie',
+    storeAccountCookie: true
+  },
   socialProviders: {
     github: {
       clientId: process.env.GITHUB_ID as string,
