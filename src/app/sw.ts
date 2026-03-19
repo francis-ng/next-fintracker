cef2b0ebb0c36de11a3b27a1c75c65eabd203012
Francis Ng <github@fnha.anonaddy.com>
2026-03-19 12:48:39 +0900

    Bump to serwist supporting turbopack

--- src/app/sw.ts
+++ src/app/sw.ts
@@ -1,12 +1,14 @@
-import { defaultCache } from "@serwist/next/worker";
-import { Serwist, PrecacheEntry } from "serwist";
+import { defaultCache } from "@serwist/turbopack/worker";
+import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
+import { Serwist } from "serwist";
 
-declare const self: ServiceWorkerGlobalScope & {
-  // Change this attribute's name to your `injectionPoint`.
-  // `injectionPoint` is an InjectManifest option.
-  // See https://serwist.pages.dev/docs/build/inject-manifest/configuring
-  __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
-};
+declare global {
+  interface WorkerGlobalScope extends SerwistGlobalConfig {
+    __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
+  }
+}
+
+declare const self: ServiceWorkerGlobalScope;
 
 const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
